e367a1bedd60b121acefbde180140f71a3b23de1
Eman-69 <emmanuelpoulose@gmail.com>
2022-10-06 18:10:00 +0530

    2

--- db.php
+++ db.php
@@ -1,8 +1,8 @@
 <?php
-	$hname="localhost";
-	$uname="Eman";
-	$password="password";
-	$db="royalenfield";
+	$hname="http://sql6.freesqldatabase.com/";
+	$uname="sql6524680";
+	$password="3pNvdyPbAQ";
+	$db="sql6524680";
 	$conn=mysqli_connect($hname,$uname,$password,$db);
 	if (!$conn) {
 		echo "Connection Failed!";
